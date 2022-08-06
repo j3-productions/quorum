214f9492300919342e92f799014bee43b7235f29
Sidnym Ladrut <sidnym@ladrut.xyz>
2022-08-06 04:21:45 +0000

    ui: MVP of front-end/back-end communication for splash screen

--- ui/src/components/Plaque.tsx
+++ ui/src/components/Plaque.tsx
@@ -4,28 +4,41 @@ import { UserRemoveIcon, UsersIcon } from '@heroicons/react/solid';
 import { deSig, uxToHex } from '@urbit/api';
 import { BoardMeta } from '../types/sphinx';
 
+// TODO: Generalize 'BoardPlaque' to cover 'ThreadMeta' props.
+// TODO: Clean up imports.
+
+// interface BoardPlaqueProps {
+//   content: BoardMeta;
+//   className?: string;
+// }
+
+interface BoardTemp {
+  name: string;
+  description: string;
+}
+
 interface BoardPlaqueProps {
-  content: BoardMeta;
+  content: BoardTemp;
   className?: string;
 }
 
-// TODO: Generalize 'BoardPlaque' to cover 'ThreadMeta' props.
-
 export const BoardPlaque = ({ content, className }: BoardPlaqueProps) => {
   /*TODO: Cleanup 'place-items-center' here; can it just be applied to img?*/
+  const tags = ["urbit", "dojo", "hoon"];
   return (
     <div className="w-full p-2 grid grid-cols-12 place-items-center gap-2 text-mauve bg-fawn/30 border-solid border-2 border-rosy rounded-xl">
       <div className="col-span-2">
         <img
-          src={content.uri}
+          src='https://lh3.googleusercontent.com/a-/AOh14GgOm4If5lPzmRZoOk5yb2TN0twuiwzp0zeJiUZ3qw=k-s256'
           className="object-cover rounded-xl"
         />
       </div>
       <div className="col-span-10 border-solid border-l-2 border-rosy px-2">
-        <a href="#" className="underline font-semibold">{content.title}</a>
+        <a href="#" className="underline font-semibold">{content.name}</a>
         <p>{content.description}</p>
         <p className="text-lavender font-semibold float-left">
-          {content.tags.map((t, i) => `${i === 0 ? '' : ' | '}#${t}`)}
+          {/* {content.tags.map((t, i) => `${i === 0 ? '' : ' | '}#${t}`)} */}
+          {tags.map((t, i) => `${i === 0 ? '' : ' | '}#${t}`)}
         </p>
       </div>
     </div>
