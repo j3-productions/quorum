ce68660fd3074a3283bd803983fce29092d465a3
Sidnym Ladrut <sidnym@ladrut.xyz>
2022-10-13 02:46:56 +0000

    ui: improve 'Plaque' rendering for better phone performance

--- ui/src/components/Plaque.tsx
+++ ui/src/components/Plaque.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import api from '../api';
 import cn from 'classnames';
 import { Link } from 'react-router-dom';
-import { sigil, reactRenderer } from '@tlon/sigil-js'
 import { Footer } from './subcomponents/Footer';
 import { MDBlock } from './subcomponents/MDBlock';
 import * as Type from '../types/quorum';
@@ -27,35 +26,20 @@ interface PlaqueProps {
 export const Plaque = ({content, className}: PlaqueProps) => {
   const imageClasses: string = `object-cover rounded-xl`;
   const plaqueClasses: string = `
-    w-full p-2 grid grid-cols-12 place-items-center gap-2
-    text-fgp1 bg-bgp2/30 border-solid border-2 border-bgs1 rounded-xl
+    w-full p-2 flex flex-col gap-2 rounded-xl text-fgp1 bg-bgp2/30
+    border-solid border-2 border-bgs1
   `;
   const truncateBody = (body: string): string =>
     body.substring(0, 200) + ((body.length > 200) ? "..." : "");
 
-  // TODO: Render image as the background of the board with light opacity,
-  // which will also require rendering the front as text with outline.
-  // TODO: Add a bottom bar to 'QuestionPlaque' that contains the vote
-  // score and reply count for a particular question.
   const BoardPlaque = ({content, className}: BoardPlaqueProps) => {
     const path: string = `/board/${content.host}/${content.name}`;
     return (
-      <div className={cn(plaqueClasses, className)}>
-        <div className="col-span-2">
-          {(content.image !== "") ?
-            (<img src={content.image} className={imageClasses} />) :
-            sigil({
-              patp: content.host,
-              renderer: reactRenderer,
-              width: 100,
-              height: 100,
-              colors: ['#586E75', '#FDF6E3'],
-              class: imageClasses,
-              attributes: {style: undefined},
-            })
-          }
-        </div>
-        <div className="col-span-10 w-full border-solid border-l-2 border-bgs1 px-2">
+      <div className={cn(plaqueClasses, "relative overflow-hidden", className)}>
+        <div className={`absolute top-0 left-0 w-full h-full opacity-30
+            bg-center bg-contain bg-repeat`}
+            style={{'background-image': `url('${content.image}`}} />
+        <div className="relative">
           <Link to={path} className="text-bgs2">
             <MDBlock content={content.name} archetype="head" />
           </Link>
@@ -70,13 +54,20 @@ export const Plaque = ({content, className}: PlaqueProps) => {
     const path: string = `/board/${content.host}/${content.board}/thread/${content.id}`;
     return (
       <div className={cn(plaqueClasses, className)}>
-        <div className="col-span-12 w-full">
+        <div>
           <Link to={path} className="text-bgs2">
             <MDBlock content={content.title} archetype="head" />
           </Link>
           <MDBlock archetype="desc" content={truncateBody(content.body)}/>
           <Footer who={content.who} host={content.host}
             date={content.date} tags={content.tags} path={path} />
+        </div>
+        <div className={`flex justify-around items-center
+            border-solid border-t border-bgs1 pt-2`}>
+          {/*TODO: On large monitors, list what each of these means w/ text*/}
+          <div>🗳: {((content.votes < 0) ? "" : "+") + content.votes}</div>
+          <div>💬: ?</div> {/*number of comments; {content.replies}*/}
+          <div>🕒: ?</div> {/*time of latest comment/update, as X ago*/}
         </div>
       </div>
     );
