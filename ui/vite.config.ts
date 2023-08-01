009949ba55e03201ac608161b45b377d8cae168c
Sidnym Ladrut <sidnym@ladrut.xyz>
2023-08-01 16:59:27 +0000

    add manual chunking of larger libraries to improve load times

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -11,7 +11,30 @@ export default ({mode}: {mode: string;}) => {
   const SHIP_URL = process.env.SHIP_URL || process.env.VITE_SHIP_URL || 'http://localhost:8080';
   console.log(SHIP_URL);
 
+  const rollupOptions = {
+    output: {
+      manualChunks: {
+        lodash: ['lodash'],
+        'lodash/fp': ['lodash/fp'],
+        '@urbit/api': ['@urbit/api'],
+        '@urbit/http-api': ['@urbit/http-api'],
+        '@tlon/sigil-js': ['@tlon/sigil-js'],
+        'any-ascii': ['any-ascii'],
+        'react-select': ['react-select'],
+        'react-hook-form': ['react-hook-form'],
+        'react-markdown': ['react-markdown'],
+        'date-fns': ['date-fns'],
+        'urbit-ob': ['urbit-ob'],
+        '@radix-ui/react-dialog': ['@radix-ui/react-dialog'],
+        '@radix-ui/react-dropdown-menu': ['@radix-ui/react-dropdown-menu'],
+        '@radix-ui/react-tooltip': ['@radix-ui/react-tooltip'],
+        '@radix-ui/react-icons': ['@radix-ui/react-icons'],
+      },
+    },
+  };
+
   return defineConfig({
+    build: { sourcemap: false, rollupOptions },
     plugins: [urbitPlugin({ base: 'quorum', target: SHIP_URL, secure: false }), reactRefresh()],
     server: { host: 'localhost', port: 3000 },
     resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
